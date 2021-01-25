b026a2da018e2658c3be05c4842c9c193540bb53
HannonN <nathan22hannon@gmail.com>
2021-01-25 16:46:24 -0500

    starting-point

---
+++ script.js
@@ -0,0 +1,10 @@
+"use Strict";
+
+const nathan = {
+    fullname: "Nathan Hannon",
+    age: 31,
+    birthday: "December 28",
+    liveInDetroit: true,
+    lifeEvents: ("I was born in Kokomo, Indiana", "I went to Ferris State University", )
+} 
+ 
